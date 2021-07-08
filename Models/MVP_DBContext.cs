e8d44c9abcea6f009ce9e550d8b4c1c4d8e77c1f
eugeneckcheah <78782607+eugeneckcheah@users.noreply.github.com>
2021-07-08 16:50:10 +0800

    Add files via upload

---
+++ Models/MVP_DBContext.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Onboard.Models
+{
+    public partial class MVP_DBContext : DbContext
+    {
+        public MVP_DBContext()
+        {
+        }
+
+        public MVP_DBContext(DbContextOptions<MVP_DBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Sale> Sales { get; set; }
+        public virtual DbSet<Store> Stores { get; set; }
+ 
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Latin1_General_CI_AI");
+
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Address)
+                    .HasMaxLength(250)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Price).HasColumnType("smallmoney");
+            });
+
+            modelBuilder.Entity<Sale>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.DateSold).HasColumnType("date");
+
+                entity.HasOne(d => d.Customer)
+                    .WithMany(p => p.Sales)
+                    .HasForeignKey(d => d.CustomerId)
+                    .HasConstraintName("FK__Sales__CustomerI__398D8EEE");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.Sales)
+                    .HasForeignKey(d => d.ProductId)
+                    .HasConstraintName("FK__Sales__ProductId__3A81B327");
+
+                entity.HasOne(d => d.Store)
+                    .WithMany(p => p.Sales)
+                    .HasForeignKey(d => d.StoreId)
+                    .HasConstraintName("FK__Sales__StoreId__3B75D760");
+            });
+
+            modelBuilder.Entity<Store>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Address).HasMaxLength(200);
+
+                entity.Property(e => e.Name).HasMaxLength(200);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Onboard.Models
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            Sales = new HashSet<Sale>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public virtual ICollection<Sale> Sales { get; set; }
+    }
+}
